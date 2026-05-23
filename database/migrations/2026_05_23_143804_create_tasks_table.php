505a6f92dc1a59c237717fd09f51024d4511c04b
Raffa <99806232+BerylazzahraR@users.noreply.github.com>
2026-05-23 21:50:25 +0700

    feat: Create migrations

---
+++ database/migrations/2026_05_23_143804_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('tasks', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('team_id')->constrained('teams')->onDelete('cascade');
+        $table->foreignId('assignee_id')->nullable()->constrained('users')->onDelete('set null');
+        $table->string('title', 255);
+        $table->text('description')->nullable();
+        $table->enum('status', ['todo', 'in_progress', 'done'])->default('todo');
+        $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
+        $table->date('deadline');
+        $table->timestamps();
+        $table->index(['team_id', 'status']);
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
